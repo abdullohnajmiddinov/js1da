23597cc07a22c59a334a31c3da3b23a79aa77707
abdullohnajmiddinov <abdullohnajmiddinov45@gmail.com>
2024-03-09 09:58:11 +0300

    done

---
+++ sal.js
@@ -0,0 +1,23 @@
+let promttext = prompt('ismingizni kiriting.')
+
+
+let promttexe = prompt('Familyangizni kiriting.')
+
+
+let salom = prompt('Yoshingizni kiriting.')
+
+let s = prompt('Yoqtirgan faningizni kiriting.')
+
+
+let sa = prompt('Yashash manzilingizni kiriting.')
+
+alert('malumotlaringiz uchun raxmat!!!!')
+
+console.log('Ism:' + promttext)
+console.log('Familya:' + promttexe)
+console.log('yosh:' + salom)
+console.log('Yoqtirgan fan:' + s)
+console.log('Yashash manzil:' + sa)
+
+
+
